1ccf9041bd96bba04bd3a24d845e52121b151269
tomojansson <56921889+tomojansson@users.noreply.github.com>
2020-04-07 15:39:32 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// Programming languages
+let name = "Tommy";
+let age = 27;
+let jobRole = "Funny Boi"
+
+console.log(name)
+console.log(`Hello, I am ${name} and I am ${age} years old.`)
+
+let numberOne;
+let numberTwo;
+
+function add(numberOne, numberTwo) {
+    let total = numberOne + numberTwo
+    console.log(total)
+}
+
+add(50, 50)
+add(70, 80)
+add(100, 50)
